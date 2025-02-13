436ceebb1504929cdf835cfcb67c03659becc864
Nikolas-Aguilar <146749353+Nikolas-Aguilar@users.noreply.github.com>
2025-02-13 13:52:50 -0300

    Enhance Book model with additional fields and validation
    - Add is_bestselling boolean field to Book model
    - Implement rating validators (1-5 range)
    - Make author field nullable
    - Add __str__ method for better model representation

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 #models are the tables in the database, where we define the columns and entities
-
 class Book(models.Model):
     title = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
-    rating = models.IntegerField()
+    author = models.CharField(null=True, max_length=100)
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    is_bestselling = models.BooleanField(default=False)
     # id = models.AutoField(primary_key=True) this is the default primary key
+
+    def __str__(self):
+        return f"{self.title} - {self.author} - {self.rating}"
